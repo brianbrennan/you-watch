3cba81d86afd8ffea69423c67970effd699b8c79
Brian Brennan <webintern@cardsandpockets.com>
2015-11-10 14:37:59 -0500

    working rough with other channels

--- app/controllers/videoController.js
+++ app/controllers/videoController.js
@@ -4,11 +4,15 @@ angular.module('videoCtrl', [])
 
 		var video_id = $location.path().substr(8,$location.path().length - 1);
 
+		console.log(video_id);
+
 		$http.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + video_id + '&key=' + $scope.settings.api_key)
 			.success(function(res){
 				if(typeof res.items !== 'undefined' && typeof res.items[0].snippet !== 'undefined'){
+					console.log(res.items[0].snippet.channelTitle);
+					console.log($scope.settings.channel);
 					if(res.items[0].snippet.channelTitle === $scope.settings.channel){
-						s('body').insert('<iframe width="800" height="450" src="http://www.youtube.com/embed/' + video_id + '?autoplay=1&showinfo=0" frameborder="0"></iframe>');
+						s('.video').insert('<iframe width="800" height="450" src="http://www.youtube.com/embed/' + video_id + '?autoplay=1&showinfo=0" frameborder="0"></iframe>');
 					} else {
 						$location.path('/404');
 					}
