1bf5a401665a87d34cc82296a5befa414edf99ca
Brian Brennan <webintern@cardsandpockets.com>
2015-11-17 16:49:10 -0500

    made playlist similar to videos

--- app/controllers/playlistsController.js
+++ app/controllers/playlistsController.js
@@ -6,12 +6,27 @@ angular.module('playlistsCtrl', [])
 				$scope.channelId = res.items[0].id;
 
 				getPlaylists();
+
+				s('.show-hide').on('click', function(){
+					getMore();
+				});
 			});
 
 		function getPlaylists(){
-			$http.get('https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2csnippet&channelId=' + $scope.channelId + '&key=' + $scope.settings.api_key)
+			$http.get('https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2csnippet&channelId=' + $scope.channelId +  '&maxResults=9&key=' + $scope.settings.api_key)
 				.success(function(res){
 					$scope.playlists = res.items;
+					$scope.nextToken = res.nextPageToken;
 				});
+		}
+
+		function getMore(){
+			$http.get('https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2csnippet&channelId=' + $scope.channelId +  '&maxResults=9&pageToken=' + $scope.nextToken + '&key=' + $scope.settings.api_key)
+				.success(function(res){
+					for(var i = 0; i < res.items.length; i++){
+						$scope.playlists.push(res.items[i]);
+					}
+					$scope.nextToken = res.nextPageToken;
+				})
 		}
 	});
--- app/controllers/videosController.js
+++ app/controllers/videosController.js
@@ -6,12 +6,27 @@ angular.module('videosCtrl', [])
 				$scope.uploadsPlaylistId = res.items[0].contentDetails.relatedPlaylists.uploads;
 
 				getVideos();
+
+				s('.show-hide').on('click', function(){
+					getMore();
+				});
 			});
 
 		function getVideos(){
-			$http.get(' https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&playlistId=' + $scope.uploadsPlaylistId + '&key=' + $scope.settings.api_key)
+			$http.get(' https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&playlistId=' + $scope.uploadsPlaylistId + '&maxResults=9&key=' + $scope.settings.api_key)
 				.success(function(res){
 					$scope.videos = res.items;
+					$scope.nextToken = res.nextPageToken;
+				})
+		}
+
+		function getMore(){
+			$http.get(' https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&playlistId=' + $scope.uploadsPlaylistId + '&maxResults=9&pageToken=' + $scope.nextToken + '&key=' + $scope.settings.api_key)
+				.success(function(res){
+					for(var i = 0; i < res.items.length; i++){
+						$scope.videos.push(res.items[i]);
+					}
+					$scope.nextToken = res.nextPageToken;
 				})
 		}
 	});
