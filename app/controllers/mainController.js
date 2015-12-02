530f152cbf36b0767bbd07d903bd8aaa81289127
Brian Brennan <webintern@cardsandpockets.com>
2015-12-02 09:01:59 -0500

    updating analytics requests

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,6 @@
 angular.module('mainCtrl', [])
 
-	.controller('mainController', function($http, $scope, $rootScope){
+	.controller('mainController', function($http, $scope, $rootScope, $window, $location){
 		$http.get('app/model/settings.json')
 			.success(function(res){
 				$scope.settings = res;
@@ -17,5 +17,9 @@ angular.module('mainCtrl', [])
 
 		$rootScope.$on('$locationChangeStart', function(event, next, current){
 			s('header').removeClass();
+		});
+
+		$rootScope.$on('$routeChangeSuccess', function(event, next, current){
+			$window.ga('send', 'pageview', { page: $location.path() });
 		});
 	});
